582c435735ff4ec7cf297afd85ffe16bd1f056e7
jpg27 <85765616+jpg27@users.noreply.github.com>
2024-05-26 16:08:26 -0500

    Add files via upload

---
+++ ProyectoDOO-feature-backend/src/test/java/com/uco/pch/PchBackend.java
@@ -0,0 +1,13 @@
+package com.uco.pch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PchBackend {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
